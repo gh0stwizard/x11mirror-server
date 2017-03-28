25d49c0bb0aaf9b3d0cbde091bfde7d1eec040c2
gh0stwizard <vitaliy.tokarev@gmail.com>
2017-03-28 16:19:02 +0300

    simplify get_current_time_string()

--- timeinfo.c
+++ timeinfo.c
@@ -6,7 +6,7 @@
 
 
 /* a precision of floating part of seconds */
-#define USEC_SIZE 6
+#define MAX_DIGITS 6
 #define NSEC_SIZE 9
 
 
@@ -15,45 +15,39 @@ get_current_time_string (char *out[], size_t out_len)
 {
 	struct timespec tp;
 	struct tm *tmp;
-	size_t len, i;
-	char usec[USEC_SIZE + 1];
-	char *usecp;
+	size_t len;
 	char nsec[NSEC_SIZE + 1];
+	long x;
 
 
 	if (clock_gettime (CLOCK_REALTIME, &tp) == -1) {
-		fprintf (stderr, "clock_gettime: %s\n",
-			strerror (errno));
+		fprintf (stderr, "%s: clock_gettime: %s\n",
+			__func__, strerror (errno));
 		return 0;
 	}
 
 	tmp = localtime (&tp.tv_sec);
 
 	if (tmp == NULL) {
-		fprintf (stderr, "localtime: %s\n",
-			strerror (errno));
+		fprintf (stderr, "%s: localtime: %s\n",
+			__func__, strerror (errno));
 		return 0;
 	}
 
+	/* fill in a date to 'out' first */
 	if (strftime (*out, out_len, TIME_FMT, tmp) == 0) {
-		fprintf (stderr, "strftime: %s\n",
-			strerror (errno));
+		fprintf (stderr, "%s: strftime: %s\n",
+			__func__, strerror (errno));
 		return 0;
 	}
 
 	/* prepend zeros */
-	snprintf (nsec, NSEC_SIZE + 1, "%li", tp.tv_nsec);
-	len = NSEC_SIZE - strlen (nsec);
-	memmove (nsec + len, nsec, len);
-	for (i = 0, usecp = nsec; i < len; i++, usecp++)
-		*usecp = '0';
-	nsec[NSEC_SIZE] = '\0';
+	memset (nsec, '0', NSEC_SIZE - 1);
+	for (len = 0, x = tp.tv_nsec; x != 0; len++, x /= 10L);
+	snprintf (nsec + NSEC_SIZE - len, len + 1, "%li", tp.tv_nsec);
 	
-	/* cut */
-	snprintf (usec, USEC_SIZE + 1, "%s", nsec);
-	
+	/* cut last digits and append the result */
 	len = strlen (*out);
-	snprintf (*out + len, DATE_SIZE - len, ".%s", usec);
-
-	return len;
+	/* MAX_DIGITS + '.' + '\0' */
+	return snprintf (*out + len, MAX_DIGITS + 2, ".%s", nsec);
 }
